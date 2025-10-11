e055f97e8de3c00f9b5ebff1f7b51e7d04508db2
aryamantepal <aryamandora@gmail.com>
2025-10-11 13:31:12 -0400

    added pdf, updated bio

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -20,11 +20,11 @@ export const profileData: ProfileData = {
     bio: [
         "I'm a junior Computer Science & Applied Mathematics major at UMass Amherst, advised by Mario Parente and Yeonsik Noh, with whom I am working on improved machine-learning methods for Mars hyperspectral data and embedded systems for underwater biomedical sensing.",
         "I've also had the pleasure of working with James Murphy at Tufts University, Jim Buffat at the Forschungszentrum Julich, Elliot Taffet at UMass Amherst and Curtis Atkisson at the University of Washington",
-        "My research interests include remote sensing, deep learning, computer vision and vision-language models."
+        "My research interests include deep learning, remote sensing, computer vision and vision-language models."
     ],
     email: "atepal@umass.edu",
     links: [
-        { name: "CV", url: "/cv.pdf" },
+        { name: "CV", url: "/pdfs/cv.pdf" },
         { name: "GitHub", url: "https://github.com/aryamantepal" },
         { name: "LinkedIn", url: "https://www.linkedin.com/in/aryamantepal/" },
     ],
